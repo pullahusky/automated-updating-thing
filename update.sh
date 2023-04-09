2e7cc08de332d0371957f36019fc4b2352f3fe1e
pullahusky <114418230+pullahusky@users.noreply.github.com>
2023-04-09 17:07:26 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,15 +1,17 @@
-
 #!/bin/bash
 
 while true; do
 
 	read -p  "Wanna update UwU y/n? " yn
 	case $yn in
-		[Yy]* ) sudo apt-get update && apt-get upgrade || echo "updated OwO"; break;;
+		[Yy]* ) sudo apt-get update -y && apt-get upgrade -y || echo "updated OwO"; break;;
+		[Yy]* ) sudo apt autoclean -y && sudo apt autoremove -y; break;;
+		[Yy]* ) sudo apt dist-upgrade -y; break;;
 		[Nn]* ) echo "Okay $USER, I won't update this time"; exit;;
 	esac
 
 done
+
 
 
 
