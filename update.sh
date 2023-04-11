d51c663a6159300c8eebc0b15e01c80b176263b2
pullahusky <114418230+pullahusky@users.noreply.github.com>
2023-04-11 13:15:10 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,16 +1,19 @@
+  GNU nano 5.4                                update.sh *                                       
 #!/bin/bash
 
 while true; do
 
-	read -p  "Wanna update UwU y/n? " yn
-	case $yn in
-		[Yy]* ) sudo apt-get update -y && apt-get upgrade -y || echo "updated OwO"; break;;
-		[Yy]* ) sudo apt autoclean -y && sudo apt autoremove -y; break;;
-		[Yy]* ) sudo apt dist-upgrade -y; break;;
-		[Nn]* ) echo "Okay $USER, I won't update this time"; exit;;
-	esac
+        read -p  "Wanna update UwU y/n? " yn
+        case $yn in
+                [Yy]* ) sudo apt-get update -y && apt-get upgrade -y || echo "updated OwO"; bre>
+                [Yy]* ) sudo apt-get autoclean -y && sudo apt-get autoremove -y; break;;
+                [Yy]* ) sudo apt-get dist-upgrade -y; break;;
+                [Nn]* ) echo "Okay $USER, I won't update this time"; exit;;
+        esac
 
 done
+
+
 
 
 
